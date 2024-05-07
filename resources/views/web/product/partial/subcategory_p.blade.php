8829bf0b7ec13a430aa82a5c6fdf34429d4cdebf
mayurgm <mayur.singh@gomilestone.com>
2024-05-07 14:56:49 +0530

    Filter code

---
+++ resources/views/web/product/partial/subcategory_p.blade.php
@@ -0,0 +1,18 @@
+@if(count($getProducts))
+
+    @foreach($getProducts as $getProduct)
+    <div class="col-md-4">
+        <div class="our-product-right-img-box">
+            <img class="our-product-img"
+                src="{{ asset($getProduct->gen_image[0] ??'assets/web/image/guest-room/guest-room-img.png') }}"
+                alt="image">
+            <a href="{{url('/product')}}/{{$getProduct->meta_url ??''}}/{{$getProduct->slug ??''}}" class="our-product-text">{{$getProduct->name ??''}}</a>
+        </div>
+    </div>
+    @endforeach
+
+@endif
+
+ @if(count($getProducts))
+    {!! $getProducts->nextPageUrl() !!} 
+@endif 
